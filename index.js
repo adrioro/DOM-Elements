4eddfcc9282671d31f5e965eaa38039573cfd7cc
adrioro <artistsvillage@me.com>
2019-01-16 17:36:42 -0500

    fifth commit

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 var element = document.getElementById("myLink");
 
-element.hasAtribute("href") //True
-element.hasAtribute("rel") //False
+
+/*True. This method returns true if the
+specified attribute exists, otherwise it returns false.*/
+element.hasAttribute("href")
+element.hasAttribute("rel") //False
+
+//Removes the string text of the tag: "LINK TO CERROTTI'S PORTFOLIO".
+element.removeAttribute("text")
+
+/*Returns the name of text string ""LINK TO..."
+if it has one. */
+element.getAttribute("text")
+
+/*setAttribute adds an attribute to the element. In this case
+it is adding an id and its value.*/
+element.setAttribute("id", "myId")
